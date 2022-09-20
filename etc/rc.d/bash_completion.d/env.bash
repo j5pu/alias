bec790c76537a295aaaa003865448eb56a5fcb53
root <root@example.com>
2022-09-20 16:31:23 +0200

    env

---
+++ etc/rc.d/bash_completion.d/env.bash
@@ -0,0 +1,36 @@
+# shellcheck shell=bash
+
+#######################################
+# alias-del completion
+# Arguments:
+#   1
+#   3
+# Returns:
+#   0 ...
+#   1 ...
+#######################################
+_env() {
+  _init_completion -n :=/ || return
+
+  test "${cword}" -lt 4 || return 0
+  [[ ! "${words[1]}" =~ -h|--help|help|hook|sync ]] || return 0
+  case "$cword" in
+    1) mapfile -t COMPREPLY < <(compgen -o nospace -W "-h --help help add clean del hook sync" -- "${cur}") ;;
+    2)
+      case "${words[1]}" in
+        add)
+          mapfile -t COMPREPLY < <(compgen -A command -- "${cur}")
+          _filedir -d
+          ;;
+        del) mapfile -t COMPREPLY < <(compgen -A alias -- "${cur}") ;;
+      esac
+      ;;
+    3)
+      if [ "${cword[2]}" != "del" ]; then
+        mapfile -t COMPREPLY < <(compgen -o nospace -W "${ENV_SUPPORTED}" -- "${cur}")
+      fi
+      ;;
+  esac
+}
+
+complete -F _env env
