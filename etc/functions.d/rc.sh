064a49b4f13587e34741f6976f91121896358a68
root <root@example.com>
2022-09-20 22:30:21 +0200

    rc

--- etc/functions.d/rc.sh
+++ etc/functions.d/rc.sh
@@ -3,22 +3,22 @@
 #
 # Functions used by env.
 
-env_source_dir() {
-  for _env_source in "$1"/*; do
-    case "${_env_source##*/}" in
-      .DS_Store | .gitkeep | .localized) continue ;;
+rc_source_dir() {
+  for _rc_source in "$1"/*; do
+    case "${_rc_source##*/}" in
+      .DS_Store | .gitkeep | .keep | .localized) continue ;;
     esac
-    . "${_env_source}"
+    . "${_rc_source}"
   done
 
-  unset _env_source
+  unset _rc_source
 }
 
 has() { command -v "$1" >/dev/null; }
 
-reenv() {
-  unset -f reenv
+rebash() {
+  unset -f rebash
   PATH="$(echo "${PATH}" | sed "s|${RC_TOP}/bin:||")"
-  unset BASH_COMPLETION_VERSINFO RC_SOURCED
+  unset BASH_COMPLETION_VERSINFO HOMEBREW_PROFILE_D_SOURCED RC_SOURCED
   . "${ENV?}"
 }
