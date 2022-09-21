009e9db40aed05e92d72f6757090f9a5d915d240
root <root@example.com>
2022-09-21 02:00:30 +0200

    rc

--- etc/functions.d/rc.sh
+++ etc/functions.d/rc.sh
@@ -21,33 +21,3 @@ resh() {
   unset BASH_COMPLETION_VERSINFO HOMEBREW_PROFILE_D_SOURCED RC_PROFILE_D_SOURCED RC_RC_D_SOURCED
   . "${ENV?}"
 }
-#commands() {
-#  brew_bin="$(brew --prefix)/bin"
-#  cd "$(brew --prefix bash-completion@2)/share/bash-completion/completions"
-#  complete -p $(find "${brew_bin}" \( -type f -or -type l \) -name 'g*' | sed "s|${brew_bin}/g||g" | sort) 2>/dev/null || true
-#}
-#export -f commands
-#bash -li -c commands
-#
-
-brew_bin="$(brew --prefix)/bin"
-commands="$(find "${brew_bin}" \( -type f -or -type l \) -name 'g*' | sed "s|${brew_bin}/g||g")"
-#
-#cd "$(brew --prefix bash-completion@2)/share/bash-completion/completions"
-#bash -li -c "complete -p ${commands}"
-
-readarray -t commands < <(find "${brew_bin}" \( -type f -or -type l \) -name 'g*' | sed "s|${brew_bin}/g||g" | grep -v "\[")
-
-cd "$(brew --prefix bash-completion@2)/share/bash-completion/completions"
-(
-  PS1='> '
-  . "$(brew --prefix)/etc/profile.d/bash_completion.sh"
-  for i in "${commands[@]}"; do
-  complete -p "${i}" 2>/dev/null | sed "s|${i}$|g${i}|g"
-done
-
-)
-#printf "%s\n" "${commands[@]}"
-#for i in "${commands[@]}"; do
-#  complete -p "${i}" 2>/dev/null | sed "s|${i}$|g${i}|g"
-#done
