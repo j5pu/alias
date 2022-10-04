666f9bf3849934e8cd87aba8e6c98ebd95da1b44
root <root@example.com>
2022-10-04 04:53:11 +0200

    auto

--- bin/stdin.sh
+++ bin/stdin.sh
@@ -4,6 +4,7 @@
 # Append stdin lines to arguments if not tty, and either stdin has content or is piped
 # Demo: /Users/Shared/JetBrains/scratch/scratches/Shell/shell/stdin show
 echo $PATH
+echo $ENV
 set -eu
 
 if { test -p /dev/stdin || test -s /dev/stdin; } && ! test -t 0; then
